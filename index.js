8f398b9861a5a1978148e9fe8115350245dfc6cb
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-10-20 23:16:43 +0600

    creating data to file utilzing filesystem

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
+const database = require('./lib/data');
 // app object - module scaffolding
 const app = {};
+
+// testing file system
+database.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangla' }, (err) => {
+  console.log(err);
+});
 
 // create server
 app.createSever = function () {
